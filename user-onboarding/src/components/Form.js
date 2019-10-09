f06af4b6c0b74591d76530ec29290da2f0cb4f3b
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-10-08 23:15:24 -0400

    Step 2 - Validation and Errors

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import {withFormik, Form, Field} from 'formik';
+import * as Yup from 'yup';
 
-function MyForm(props) {
+function MyForm({values, errors, touched}) {
   return (
     <Form>
-      <Field type="text" name="name" placeholder="Name" />
-      <Field type="email" name="email" placeholder="Email" />
-      <Field type="password" name="password" placeholder="Password" />
-      {/* Terms of Service Checkbox */}
+      <div>
+        {touched.name && errors.name && <p>{errors.name}</p>}
+        <Field type="text" name="name" placeholder="Name" />
+      </div>
+      <div>
+        {touched.email && errors.email && <p>{errors.email}</p>}
+        <Field type="email" name="email" placeholder="Email" />
+      </div>
+      <div>
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field type="password" name="password" placeholder="Password" />
+      </div>
+      <label>
+        <Field type="checkbox" name="tos" checked={values.tos} />
+        Accept Terms of Service
+      </label>
       <button>Submit</button> 
     </Form>
   );
 }
 
 const LoginForm = withFormik({
-  mapPropsToValues({ name, email, password }) {
+  mapPropsToValues({ name, email, password, tos }) {
     return {
       name: name || "",
       email: email || "",
-      password: password || ""
+      password: password || "",
+      tos: tos || false
     };
   },
+
+  // Validation Schema
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+    // Create custom error codes
+      .max(40, "Please enter no more than 40 characters for your name")
+      .required("Name is required"),
+    email: Yup.string()
+    // Create custom error codes
+      .email("Email is not valid")
+      .required("Email is required"),
+    password: Yup.string()
+    // Create custom error codes
+      .min(6, "Password must be 6 characters or longer")
+      .required("Password is required")
+  }),
+  // End Validation Schema
 
   handleSubmit(values) {
     console.log(values);
