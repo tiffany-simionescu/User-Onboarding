7c3285a0ffb70340dbebba1be68a98db1662a54f
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-10-08 22:36:19 -0400

    Initial Commit - Finished Set Up

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Form from './components/Form.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Form />
+      </header>
+    </div>
+  );
+}
+
+export default App;
