f0816fbf67e6f0ac4a111b874f6f18d13b9d2bcb
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-10-08 22:52:08 -0400

    Step 1 - Create Your Formik Form

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import {withFormik, Form, Field} from 'formik';
 
-export default function Form(props) {
+function MyForm(props) {
   return (
-    <div></div>
+    <Form>
+      <Field type="text" name="name" placeholder="Name" />
+      <Field type="email" name="email" placeholder="Email" />
+      <Field type="password" name="password" placeholder="Password" />
+      {/* Terms of Service Checkbox */}
+      <button>Submit</button> 
+    </Form>
   );
 }
+
+const LoginForm = withFormik({
+  mapPropsToValues({ name, email, password }) {
+    return {
+      name: name || "",
+      email: email || "",
+      password: password || ""
+    };
+  },
+
+  handleSubmit(values) {
+    console.log(values);
+  }
+})(MyForm);
+
+export default LoginForm;
